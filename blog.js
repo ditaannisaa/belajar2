bfc43c518da39b13ed578bfc5dc4cf444703d84f
ditaannisa <ditaa.annisa@gmail.com>
2023-08-12 17:06:08 +0700

    taskday5

--- blog.js
+++ blog.js
@@ -15,22 +15,51 @@ function addBlog(event) {
 
   image = URL.createObjectURL(image[0]);
   console.log(image);
+  
+  let start = new Date(startDate);
+  let end = new Date(endDate);
 
-  let nodeJsLogo = nodeJs
-  let reactJsLogo = reactJs
-  let nextJsLogo = nextJs
-  let typescriptLogo = typescript
+  let difference = end.getTime() - start.getTime();
+  let day = difference / (1000 * 3600 * 24);
+  let week = Math.floor(day / 7);
+  let month = Math.floor(week / 4);
+  let year = Math.floor(month / 12);
+  let durasi = "";
+
+  // console.log(difference)
+  // console.log(day)
+  // console.log(week)
+  // console.log(month)
+  // console.log(year)
+
+  if (day > 0){
+    durasi = day + " hari";
+  } 
+  if (week > 0){
+    durasi = week + " minggu";
+  } 
+  if (month > 0){
+    durasi = month + " bulan";
+  } 
+  if (year > 0){
+    durasi = year + " tahun";
+  } 
+  // console.log(durasi)
+
+  let nodeJsLogo = nodeJs;
+  let reactJsLogo = reactJs;
+  let nextJsLogo = nextJs;
+  let typescriptLogo = typescript;
 
   let blog = {
     project,
-    startDate,
-    endDate,
     description,
     reactJsLogo,
     nodeJsLogo,
     nextJsLogo,
     typescriptLogo,
     image,
+    durasi,
   };
 
   dataBlog.push(blog);
@@ -55,14 +84,14 @@ function renderBlog() {
     document.getElementById("contents").innerHTML += `
               <div class="card-item">
                     <div class="blog-image">
-                        <img src="${dataBlog[index].image}" alt="" style="width: 100%;">
+                        <img src="${dataBlog[index].image}" alt="" style="width: 100%; height: 50%;">
                     </div>
                     <div class="blog-content">
                         <h4 style="height: fit-content;">
                             <a href="blog-details.html">${dataBlog[index].project}</a>
                         </h4>
                         <div class="blog-details">
-                        durasi : 3 bulan
+                        durasi : ${dataBlog[index].durasi}
                         </div>
                         <div>
                         <p style="font-size: 12px; font-weight: 400; text-align: justify;">
@@ -70,14 +99,14 @@ function renderBlog() {
                         </p>
                         </div>
                         <div class="application">
-                            <i class="fa-brands fa-react" hidden id="react"></i>
-                            <i class="fa-brands fa-node-js" hidden id="node"></i>
+                            <img src="icons8-react-js.svg" id="react" style="height: 15px; width: 15px;" hidden>
+                            <img src="icons8-nodejs.svg" id="node" style="height: 15px; width: 15px;" hidden>
                             <img src="next-js.svg" alt="" id="next" style="height: 15px; width: 15px;" hidden>
                             <img src="icons8-typescript.svg" alt="" id="type" style="height: 15px; width: 15px;" hidden>
                         </div>
-                        <div class="btn-group">
-                            <button class="btn-edit">Edit</button>
-                            <button class="btn-delete">Delete</button>
+                        <div class="btn-group" >
+                            <button class="btn-edit" >Edit</button>
+                            <button class="btn-delete" >Delete</button>
                         </div>
                     </div>
               </div>
