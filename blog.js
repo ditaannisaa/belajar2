c5aad1f12b77fa82a2f09d05af2366163035ac36
ditaannisa <ditaa.annisa@gmail.com>
2023-08-16 08:28:37 +0700

    newtaskday6

--- blog.js
+++ blog.js
@@ -106,3 +106,18 @@ function renderBlog() {
   } 
  
 }
+
+let hamburgerIsOpen = false;
+
+const openHamburger = () =>{
+  let hamburgerNavCon = document.getElementById("hamburger-nav-container") ;
+
+  if (!hamburgerIsOpen) {
+    hamburgerNavCon.style.display = "block";
+    hamburgerIsOpen = true
+  }
+    else {
+      hamburgerNavCon.style.display = "none";
+      hamburgerIsOpen = false
+    }
+}
